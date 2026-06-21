cc508bb0cfb1777fd5efa15cec354d69d32d8172
Prathmesh Shahapure <prathmeshshahapure28@gmail.com>
2026-06-21 12:04:35 +0530

    feat: implement user registration endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 dotenv.config();
 
+connectDB();
 const PORT=process.env.PORT ;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
