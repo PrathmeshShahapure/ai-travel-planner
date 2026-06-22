a284bfc897e06dad082cef1b5dcf334ea5a26b3c
Prathmesh Shahapure <prathmeshshahapure28@gmail.com>
2026-06-23 01:27:21 +0530

    fix: update live url

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-travel-planner-backend-moji.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
